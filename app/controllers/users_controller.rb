26e631da23770c81cd20bad3aa727d877bba7a20
KevinMOGrady <kevin.ogradyk@gmail.com>
2016-04-18 20:53:39 +0000

    Add edit update functionality and partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :show, :destroy]
+   
   def new
     @user = User.new
   end
@@ -6,15 +8,40 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcom to the alpha blog #{@user.username}"
+      flash[:success] = "Welcome to the alpha blog #{@user.username}"
       redirect_to articles_path
     else
       render 'new'
     end
   end
   
+  def edit
+    # @user = User.find(params[:id])
+  end
+  
+  def update
+    if @user.update(user_params)
+      flash[:success] = 'Your account was updated successfully'
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+  
+  def show
+    
+  end
+  
+  def destroy
+    
+  end
+  
   private
     def user_params
       params.require(:user).permit(:username, :email, :password)
+    end
+    
+    def set_user
+      @user = User.find(params[:id])
     end
 end
